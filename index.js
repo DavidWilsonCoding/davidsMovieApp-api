fd51b35903644f875e336de651a7360f5e1419ae
David Wilson <david.wilson.coding@gmail.com>
2022-04-15 10:41:42 +0100

    Reenabled jwt auth on get movies endpoint

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(express.static('public'));
 
 //passport.authenticate('jwt', { session: false }), 
 //GET all movies
-app.get ('/movies', (req, res) => {
+app.get ('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(200).json(movies);
